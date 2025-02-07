64ed945c2993a5208f8f0b70e0a2af46511b9ef5
Diego Olvera <omega_717@hotmail.com>
2025-02-07 17:04:32 +0100

    Update __init__.py
    update init

--- qadaptive/__init__.py
+++ qadaptive/__init__.py
@@ -1,4 +1,3 @@
-# qadaptive/__init__.py
 """
 QAdaptive: A package for adaptive variational quantum circuits.
 """
@@ -6,8 +5,6 @@ QAdaptive: A package for adaptive variational quantum circuits.
 __version__ = "0.1.0"
 
 from .adaptive_ansatz import AdaptiveAnsatz
-from .mutable_optimizer import MutableOptimizer
-from .operator_pool import OperatorPool
 
-__all__ = ["AdaptiveAnsatz", "MutableOptimizer", "OperatorPool"]
+__all__ = ["AdaptiveAnsatz"]
 __version__ = "0.1.0"
