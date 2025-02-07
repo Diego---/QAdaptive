314514cca4e71be1f928a2c39ee9fa344e18bdc7
Diego Olvera <omega_717@hotmail.com>
2025-02-07 19:44:42 +0100

    add a test for initializing an AdaptiveAnsatz
    add a test for initializing an AdaptiveAnsatz

--- qadaptive/tests/test_adaptive_ansatz.py
+++ qadaptive/tests/test_adaptive_ansatz.py
@@ -1,10 +1,68 @@
 import pytest
-from qiskit import QuantumCircuit
+import numpy as np
+
+from qiskit.circuit import QuantumCircuit, ParameterVector
 from qiskit.circuit.library.standard_gates import get_standard_gate_name_mapping
 
 from qadaptive.adaptive_ansatz import AdaptiveAnsatz
 
 INSTRUCTION_MAP = get_standard_gate_name_mapping()
+
+th = ParameterVector('t', 20)
+pi = np.pi
+ansatz = QuantumCircuit(3)
+
+ansatz.ry(0.5*pi, 1)
+ansatz.rx(pi, 1)
+ansatz.cz(0,1)
+ansatz.ry(0.5*pi, 1)
+ansatz.rx(pi, 1)
+
+ansatz.ry(0.5*pi, 2)
+ansatz.rx(pi, 2)
+ansatz.cz(0,2)
+ansatz.ry(0.5*pi, 2)
+ansatz.rx(pi, 2)
+
+ansatz.ry(th[0], 0)
+ansatz.rx(th[1], 0)
+
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.cz(0,1)
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.rz(th[2], 0)
+
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.cz(0,2)
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.rz(th[3], 0)
+
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.cz(0,1)
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.rz(th[4], 0)
+
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.cz(0,2)
+ansatz.ry(0.5*pi, 0)
+ansatz.rx(pi, 0)
+ansatz.rz(th[5], 0)
+
+ansatz.ry(th[6], 0)
+ansatz.rx(th[7],0)
+
+def test_adaptive_ansatz_initialization():
+    """Test that AdaptiveAnsatz correctly initializes with a complex ansatz."""
+    adaptive_ansatz = AdaptiveAnsatz(ansatz)
+    assert len(adaptive_ansatz.get_current_ansatz().parameters) == len(th)
+
 
 def test_add_gate_at_index():
     """Test that adding a gate at a specific index correctly modifies the circuit."""
