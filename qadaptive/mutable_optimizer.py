9dd08c2c5550a6d4041023d3dcab438f7e94f0f7
Diego Olvera <omega_717@hotmail.com>
2025-02-10 22:12:53 +0100

    implement train_one_time method
    Implement a method for training once and make corresponding adjustments and add needed class attributes

--- qadaptive/mutable_optimizer.py
+++ qadaptive/mutable_optimizer.py
@@ -1,15 +1,18 @@
 import numpy as np
+import logging
 from time import time
 from typing import Callable, SupportsFloat
 
-from qiskit.circuit import Parameter
-from qiskit_algorithms.optimizers.optimizer import Optimizer
+from qiskit.circuit import Parameter, QuantumCircuit
+from qiskit_algorithms.optimizers.optimizer import Optimizer, OptimizerResult
 
 from qae.optimization.my_spsa import SPSA
 from qadaptive.adaptive_ansatz import AdaptiveAnsatz
 
 CALLBACK = Callable[[int, np.ndarray, float, SupportsFloat, bool], None]
 TERMINATIONCHECKER = Callable[[int, np.ndarray, float, SupportsFloat, bool], bool]
+
+logger = logging.getLogger(__name__)
 
 class MutableOptimizer:
     """
@@ -40,7 +43,7 @@ class MutableOptimizer:
 
     def __init__(
         self, 
-        ansatz: AdaptiveAnsatz, 
+        adaptive_ansatz: AdaptiveAnsatz, 
         optimizer: Optimizer | None = None, 
         track_gradients: bool = True,
         callback: CALLBACK | None = None,
@@ -77,21 +80,24 @@ class MutableOptimizer:
         ``compute_loss_and_gradient_estimate``, ``process_update``
         If they don't have them, they must be implemented.
         """
-        self.ansatz = ansatz
+        self.adaptive_ansatz = adaptive_ansatz
+        self.ansatz: QuantumCircuit = adaptive_ansatz.get_current_ansatz()
         self._current_iteration = 0
         self.optimizer = SPSA(**optimizer_options) if optimizer is None else optimizer
         self.track_gradients = track_gradients
-        self.gradient_history = [] if track_gradients else None
+        self.gradient_history = {0 : []} if track_gradients else None
         self.iteration = 0
         self.callback = callback
         self.termination_checker = termination_checker
+        self._times_trained = 0
 
     def step(
         self, 
         loss_function: Callable[[np.ndarray], float], 
         x: np.ndarray, 
-        loss_next: Callable[[np.ndarray], float] | None = None
-        ) -> tuple[bool, np.ndarray, float]:
+        loss_next: Callable[[np.ndarray], float] | None = None,
+        **kwargs
+        ) -> tuple[bool, np.ndarray, float, float]:
         """
         Perform a single optimization step.
 
@@ -118,11 +124,8 @@ class MutableOptimizer:
         x = np.asarray(x)
                 
         fx_estimate, gradient_estimate = self.optimizer.compute_loss_and_gradient_estimate(
-            loss_function, x
+            loss_function, x, **kwargs
             )
-        
-        if self.track_gradients:
-            self.gradient_history.append(gradient_estimate)
 
         skip, x_next, fx_next = self.optimizer.process_update(
             gradient_estimate, x, fx_estimate, loss_next, iteration_start, self._current_iteration
@@ -141,19 +144,19 @@ class MutableOptimizer:
                 skip
                 )
             
-        return skip, x_next, fx_next
+        return skip, x_next, fx_next, gradient_estimate, fx_estimate
 
-    def compute_gradients(self) -> np.ndarray:
+    def get_gradients(self) -> np.ndarray:
         """
-        Compute gradients of the cost function with respect to ansatz parameters.
+        Get gradients of the cost function with respect to ansatz parameters.
 
         Returns
         -------
         np.ndarray
-            The computed gradient values.
+            The current estimated gradient values.
         """
         # Placeholder for actual gradient computation
-        return np.random.randn(len(self.ansatz.param_vector))
+        return np.random.randn(len(self.adaptive_ansatz.param_vector))
 
     def update_parameters(self, gradients: np.ndarray):
         """
@@ -167,18 +170,147 @@ class MutableOptimizer:
         # Placeholder: Apply optimizer update
         pass
 
-    def train(self, iterations: int = 100):
+    def train_one_time(
+        self, 
+        loss_function: Callable[[np.ndarray], float],
+        initial_point: list | np.ndarray | None = None,
+        loss_next: Callable[[np.ndarray], float] | None = None,
+        iterations: int = 100,
+        adaptation_callback: Callable | None = None,
+        **kwargs
+        ) -> OptimizerResult:
         """
         Train the ansatz for a given number of iterations.
 
         Parameters
         ----------
+        loss_function : Callable[[np.ndarray], float]
+            The cost function to evaluate the ansatz.
         iterations : int
             The number of optimization steps.
+            
+        Returns
+        ----------
+        OptimizerResult
+            The result of the optimization.
         """
-        for _ in range(iterations):
-            self.step()
+        logger.info(f"Started minimization of loss funcion. Repetitions: {self._times_trained}.")
+        
+        if self.optimizer.blocking:
+            raise NotImplementedError("Training with blocking is not yet implemented.")
+        
+        if initial_point is None:
+            initial_point = np.random.normal(loc=0, scale=1, size=len(self.ansatz.parameters))
+        x = np.asarray(initial_point)
+        
+        # If the iterators have not been set, set them now.
+        if self.optimizer.p_iterator is None and self.optimizer.lr_iterator is None:
+            self.optimizer._create_iterators(loss_function, initial_point)
+        
+        # Set up optimizer for epoch mode or iteration mode
+        use_epochs = kwargs.get('use_epochs')
+        if use_epochs:
+            logger.info(f"Starting optimization with initial parameters {x} doing epochs.")
+            logger.info("Interpreting max number of iterations as max number of epochs.")
+        else:
+            logger.info(f"Starting optimization with initial parameters {x}.")
+        
+        ncpg = kwargs.get('num_circs_per_group')
+        ncpb = kwargs.get('num_circs_per_batch')
+        if use_epochs:
+            # Total number of circuits (complete data set)
+            total_circuits = self.optimizer._size_full_batch if self.optimizer._size_full_batch else 12
+            if not ncpb:
+                ncpb = 3
+        logger.info(f"Setting number of circuits per batch to {ncpb}.")
+        # use a local variable and while loop to keep track of the number of iterations
+        # if the termination checker terminates early
+        start = time()
+        k = 0
+        logger.info("Starting optimization loop")
+        while k < iterations:
+            k += 1
+            current_learn_rate = next(self.optimizer._lr_iterator_copy)
             self.iteration += 1
+            iteration_start = time()
+            # Compute updates for the whole batched dataset when using epochs
+            if use_epochs:
+                # Generate indices for all circuits
+                indices = np.arange(total_circuits)
+                # Shuffle indices for randomness
+                np.random.shuffle(indices)
+                # Split indices into batches
+                batch_indices = [indices[i:i + ncpb] for i in range(0, total_circuits, ncpb)]
+                for i, indices in enumerate(batch_indices):
+                    logger.info(f"Evaluating batch {i+1} out of {len(batch_indices)}.")
+                    skip, x_next, fx_next, gradient_estimate, fx_estimate = self.step(
+                        loss_function, x, loss_next, used_circs_indices = indices
+                        )
+                    if skip:
+                        continue
+                    # Update values
+                    x = x_next
+                logger.info(f"Epoch {k}/{iterations} finished in {time() - iteration_start}")
+            # Compute updates iteration by iteration
+            else:
+                skip, x_next, fx_next, gradient_estimate, fx_estimate = self.step(
+                    loss_function, x, loss_next, num_circs_per_group = ncpg
+                    )
+                if skip:
+                    continue
+                # Update values
+                x = x_next
+                logger.info(f"Iteration {k}/{iterations} done in {time()-iteration_start}.")
+            
+            logger.info("Running the optimizer's callback.")
+            # Run the optimizer's callback
+            if self.optimizer.callback is not None:
+                if loss_next is  None:
+                    logger.info("Calculating next step for the callback, which takes another function evaluation.")
+                    self.optimizer._nfev += 1
+                    fx_next = loss_function(x_next)
+                else:
+                    logger.info("Calculating next step for the callback with custom function.")
+                    self.optimizer._nextfev += 1
+                    fx_next = loss_next(x_next)
+                    
+                self.optimizer.callback(
+                    self.optimizer._nfev,  # number of function evals
+                    x_next,  # next parameters
+                    fx_next,  # loss at next parameters
+                    np.linalg.norm(gradient_estimate*current_learn_rate),  # size of the update step
+                    True, # accepted
+                )
+            
+            # Update gradient history
+            if self.track_gradients:
+                self.gradient_history[self._times_trained].append(gradient_estimate)
+
+            if self.optimizer.termination_checker is not None:
+                fx_check = fx_estimate if fx_next is None else fx_next
+                if self.optimizer.termination_checker(
+                    self.optimizer._nfev, x_next, fx_check, np.linalg.norm(gradient_estimate*current_learn_rate), True
+                ):
+                    logger.info(f"terminated optimization at {k}/{iterations} iterations")
+                    break
+
+        logger.info("SPSA: Finished in %s", time() - start)
+        
+        self._times_trained += 1 
+        if self.track_gradients:
+            self.gradient_history[self._times_trained] = []
+            
+        result = OptimizerResult()
+        result.x = x
+        if loss_next is None:
+            logger.info("Calculating cost funtion value for final parameters.")
+        else:
+            logger.info("Calculating custom cost funtion value for final parameters.")
+        result.fun = loss_function(x) if loss_next is None else loss_next(x)
+        result.nfev = self.optimizer._nfev
+        result.nit = k
+
+        return result
 
     def prune_and_grow(self):
         """
