525ccb152099f6a2dc4fc3df56c55fcefaf1693e
Diego Olvera <omega_717@hotmail.com>
2026-04-12 11:23:42 +0200

    reduce number of entangling gates in the default pool from 2 to 1

--- qadaptive/core/operator_pool.py
+++ qadaptive/core/operator_pool.py
@@ -81,12 +81,11 @@ def _build_cx_identity(params: list[Parameter]) -> QuantumCircuit:
     For all parameters initialized to zero, the block is exactly identity.
     """
     qc = QuantumCircuit(2, name="cx_identity")
-    qc.cx(0, 1)
     qc.rz(params[0], 0)
-    qc.rx(params[1], 0)
-    qc.rz(params[2], 1)
-    qc.rx(params[3], 1)
+    qc.ry(params[1], 0)
     qc.cx(0, 1)
+    qc.rz(params[2], 1)
+    qc.ry(params[3], 1)
     return qc
 
 def _build_cz_identity(params: list[Parameter]) -> QuantumCircuit:
@@ -104,7 +103,6 @@ def _build_cz_identity(params: list[Parameter]) -> QuantumCircuit:
     For all parameters initialized to zero, the block is exactly identity.
     """
     qc = QuantumCircuit(2, name="cz_identity")
-    qc.cz(0, 1)
     qc.ry(params[0], 0)
     qc.rx(params[1], 0)
     qc.cz(0, 1)
