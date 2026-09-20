a94445039e26afd713def3cfa67ca91c1ef38836
Diego Olvera <omega_717@hotmail.com>
2026-09-20 14:53:43 +0200

    remove extra version of create_callback_args

--- qadaptive/training/optimizers/__init__.py
+++ qadaptive/training/optimizers/__init__.py
@@ -1,5 +1,4 @@
 from .spsa import SPSA, powerseries
 from .adam import ADAM
-from .utils import create_callback_args
 
-__all__ = ["SPSA", "ADAM", "powerseries", "create_callback_args"]
+__all__ = ["SPSA", "ADAM", "powerseries"]
